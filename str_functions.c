32bab99ac716cf780bee5a09e436062a53fc0319
Anela <Anelamade@gmail.com>
2023-05-16 09:44:01 -0700

    My shell commits

---
+++ str_functions.c
@@ -0,0 +1,87 @@
+/*
+ * File: str_function1.c
+ * Auth: Anela Made & Mark Oladeinde
+ */
+
+#include "main.h"
+
+int_strlen(const char *s);
+char *_strcpy(char *dest, const char *src);
+char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, const char *src, size_t n);
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: A poiint to the characters string.
+ *
+ * Return: The length of the character string.
+ */
+int _strlen(const char *s)
+{
+	int length = 0;
+
+	if (!s)
+		return (length);
+	for (length = 0; s[length]; length++)
+		return(length);
+}
+
+/**
+ * _strcpy - Copies the string pointed to by src, including the terminsting null byte, to the buffer, pointed by destination.
+ * @dest: Pointer to the destination of the copied string.
+ * @src: Pointer to the src of the source string.
+ *
+ * Return: Pointer to destination.
+ */
+char *_strcpy(char *dest, const char *src)
+{
+	size_t i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: Pointer to the destination string.
+ * @src: Pointer to the source string.
+ *
+ * Return: Pointer to the destination string.
+ */
+char *_strcat(char *dest, const char *src)
+{
+	char *destTemp;
+	const char *srcTemp;
+
+	destTemp = dest;
+	srcTemp = src;
+
+	while(*destTemp != '\0')
+		destTemp++;
+	while (srcTemp != '\0')
+		*destTemp++ =  *srcTemp++;
+	*destTemp = '\0';
+	return (dest);
+}
+
+/**
+ * _strncat - Concatenates two strings where n number of bytes are copied from source.
+ * @dest: Pointer to the destination string.
+ * @src: Pointer to the source string.
+ * @n: n bytes to copy from source.
+ *
+ * Return: Pointer to the destination string.
+ */
+char *_strncat(char *dest, const char *src, size_t n)
+{
+	size_t dest_len = _strlen(dest);
+	size_t i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}
---
+++ str_functions2.c
@@ -0,0 +1,103 @@
+/*
+ * File: str_functions2.c
+ * Auth: Anela Made & Mark Oladeinde
+ */
+
+#include "main.h"
+
+char *_strchr(char *s, char c);
+int _strspn(char *s, char *accept);
+int _strcmp(char *s1, char *s2);
+int _strncmp(const char *s1, const char *s2, size_t n);
+
+/** _strchr - Locates a character in a string.
+ * @s: the string to be searched.
+ * @c: The character to be located.
+
+ * Return: If c is found - A pointer to the first occurence.
+ * If C is not found - NULL.
+ */
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index]; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
+	return (NULL);
+}
+
+/**
+ * _strcpn - Gets the length of the prefix substring.
+ * @s: The string to be searched.
+ * @accept: The prefix to be measured.
+ *
+ * Return: the number of bytes in s which consist only of bytes from accept.
+ */
+int _strspn(char *s, char *accept)
+{ 
+	int bytes = 0;
+	int index;
+
+	while (*s)
+	{
+		for (ndex = 0; accept[index]; index++)
+		{
+			is (*s == accept[index])
+			{
+				bytes++;
+				break;
+			}
+		}
+		s++;
+	}
+	return (bytes);
+}
+/**
+ * _strcmp - Compares two strings.
+ * @s1: The first string to be compared.
+ * @s2: The second string to be compared.
+ *
+ * Return: Positive byte difference if s1 > s2
+ * 0 if s1 = s2
+ * negative byte difference if s1 < s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 ==*s2)
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 != *s2)
+		rerurn (*s1 - *s2);
+	return (0);
+}
+
+/**
+ * _strncmp - Compare two strings.
+ * @s1: Pointer to a string.
+ * @s2: Pointer to a string.
+ * @n: The first n bytes of the strings to compare.
+ *
+ * Return: Less than 0 if s1 is shorter than s2.
+ * 0 if s1 and s2 match.
+ * Greater than 0 if s1 is longer than s2.
+ */
+int _strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t i;
+	for (i = 0; s1[i] && s2[i] && i < n; i++)
+	{
+		if (s1[i] > s2[i])
+			return (s1[i] - s2[i];
+					else if (s1[i] < s2[i])
+					return (s1[i] - s2[i]);
+					}
+					if (i == n)
+					return (0);
+						else
+						return (-15);
+}
