70d1ed16dbda8029c6280fba81c0af5a100703ff
Moschifire <markoladeinde@gmail.com>
2023-05-24 20:26:56 +0100

    corrections

--- builtin.c
+++ builtin.c
@@ -80,7 +80,7 @@ int (*get_builtin(char *command))(char **args, char **front)
  *	-1 if an error occurs
  *	Otherwise - 0
  */
-int shellby_cd(char **agrs, char __attribute__((__unused__)) **front)
+int shellby_cd(char **args, char __attribute__((__unused__)) **front)
 {
 	char **dir_info, *new_line = "\n";
 	char *oldpwd = NULL, *pwd = NULL;
